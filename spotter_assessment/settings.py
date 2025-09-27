7e60ec659b044980e2a87ee89d03ea20a97dd600
Priscilla Mikisa <priscillamikisa@gmail.com>
2025-09-27 17:41:05 +0300

    added render deployments

--- spotter_assessment/settings.py
+++ spotter_assessment/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from decouple import config
 import logging.config
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
